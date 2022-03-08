ffde68571ec112945ec0fcdcf102494c28dd3b01
eliwuu <marekwilk@gmail.com>
2022-03-08 10:41:32 +0100

    fix

--- img-run.sh
+++ img-run.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-qemu-system-x86_64 -m 1G \
--nic user -boot d -cdrom alpine-virt-3.15.0-x86_64.iso \
--hda alpine.qcow2 -display curses -nographic -accel hvf
+qemu-system-x86_64 -m 1G -nic user -boot d -cdrom alpine-virt-3.15.0-x86_64.iso -hda alpine.qcow2 -display curses -nographic -accel hvf -netdev user,id=mynet0,hostfwd=tcp::5007-:5007 -device e1000,netdev=mynet0
